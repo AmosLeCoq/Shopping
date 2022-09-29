30cf464b27e72fa0311f9b01cf6c8f9062a0389d
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-09-29 16:13:28 +0200

    Move Docs Folder

---
+++ TestShopping/IntegrationShopping.cs
@@ -0,0 +1,16 @@
+namespace TestShopping
+{
+    public class TestArticle
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
